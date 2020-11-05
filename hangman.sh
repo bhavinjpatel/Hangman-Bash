aa28fe6b4cff30c38e01a1c0b16b37e22098b4cf
Dylan Klintworth <dylan.klintworth@gmail.com>
2020-11-04 23:18:48 -0600

    changed menu navigation

--- hangman.sh
+++ hangman.sh
@@ -16,32 +16,36 @@ USEROPTIONS="
 3) Contribution Details
 4) Quit
 "
-menu () {
-
-	echo "$USERMENU"
-	read -p "Please enter an option: " USEROPTION
-
-	while [[ ( -z "$USEROPTION") || (! "$USEROPTION" =~ ^[1-4]{1}$) ]];
-	do
-		echo "$USEROPTION is not a valid option."
-		echo "$USEROPTIONS"
-		read -p "Please enter an option: " USEROPTION
-	done
-return
+go_back () {
+	read -p "Enter 'gb' to go back: " GB
+	if [[ "$GB" == "gb" ]]; then
+		display_menus
+	fi
 }
 display_menus () {
 
+echo "$USERMENU"
+read -p "Please enter an option: " USEROPTION
+
+while [[ ( -z "$USEROPTION") || (! "$USEROPTION" =~ ^[1-4]{1}$) ]];
+do
+	echo "$USEROPTION is not a valid option."
+	echo "$USEROPTIONS"
+	read -p "Please enter an option: " USEROPTION
+done
 if [[ "$USEROPTION" -eq 1 ]]; then
 	WORD=$(shuf ./assets/words.txt | head -n 1)
 	echo $WORD
+	go_back
 elif [[ "$USEROPTION" -eq 2 ]]; then
 	echo "Help Menu:"
+	go_back
 elif [[ "$USEROPTION" -eq 3 ]]; then
 	echo "Project by: Bhavin Patel, Christian Santana, and Dylan Klintworth"
+	go_back
 else
-	echo "Quitting hangman"
+	echo "Quitting Hangman"
 	exit 1
 fi
-return
 }
-menu
+display_menus
